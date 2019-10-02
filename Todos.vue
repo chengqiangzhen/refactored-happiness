3246bbb7317463c8a537297523b52f9efa18c3c5
chengqiangzhen <56057223+chengqiangzhen@users.noreply.github.com>
2019-10-02 12:39:23 +0800

    上传vue文件
    第一次用GitHub上传文件

---
+++ Todos.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+       <h1>我是todos</h1>
+       <p v-for="(item,index) in t" :key="index">用户id:{{item.id}}, 标题：{{item.title}}</p>
+   </div>
+</template>
+
+<script>
+    
+    import {mapGetters} from 'vuex'
+    export default {
+        name: "Todos",
+        computed: mapGetters({
+            t: "getAllTodos"
+        })
+    }
+</script>
+
+<style>
+    
+</style>
